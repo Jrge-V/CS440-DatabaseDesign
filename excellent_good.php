b0d9838964abc9a655eeed7b74d004d91a1226bd
Jrge-V <jorgeverduzco91@gmail.com>
2023-04-20 07:33:13 -0700

    fixed issue where invalid username would crash the site

--- excellent_good.php
+++ excellent_good.php
@@ -28,62 +28,63 @@ if (isset($_POST["username"])) {
     $sql = "SELECT id FROM items WHERE username = '$item_username'";
     $result = $link->query($sql);
 
-    // Get the ids associated with the username
-    $item_ids = array();
+    // Check if the query returned any rows
     if (mysqli_num_rows($result) > 0) {
+
+        // Get the ids associated with the username
+        $item_ids = array();
         while ($row = mysqli_fetch_assoc($result)) {
             $item_ids[] = $row["id"];
         }
-    }
 
-    // Build the query to fetch the items that meet the criteria
+        // Build the query to fetch the items that meet the criteria
 
-    // Items with only "Excellent" and "Good" not "bad" and "fair"
-    // Use gary for excellent and good
-    //use joe for an excellent and a bad
-    $sql = "SELECT DISTINCT items.title FROM items
-    INNER JOIN reviews ON items.id = reviews.item_id
-    WHERE items.id IN (" . implode(",", $item_ids) . ")
-    AND (
-        reviews.review LIKE '%Excellent%' OR
-        reviews.review LIKE '%Good%'
-    )
-    AND items.id NOT IN (
-        SELECT item_id FROM reviews
-        WHERE review LIKE '%Bad%' OR review LIKE '%Fair%'
-    )";
-
-
-        // Output the results
-    //  Use: neil, items 2(ipad/poor, excellent,good) and 6(google pixel 6/good, exccelent) | gary, items 4(Apple TV/excellent, excellent) and 5(Samsung 23/poor) | rick for no items with good or excellent ratings.
-
-    // $sql = "SELECT DISTINCT items.title FROM items
-    // INNER JOIN reviews ON items.id = reviews.item_id
-    // WHERE items.id IN (" . implode(",", $item_ids) . ")
-    // AND reviews.rating IN ('excellent', 'good')
-    // AND items.id NOT IN (
-    //     SELECT item_id FROM reviews
-    //     WHERE rating IN ('fair', 'poor')
-    // )";
+        // Items with only "Excellent" and "Good" not "bad" and "fair"
+        // Use gary for excellent and good
+        //use joe for an excellent and a bad
+        $sql = "SELECT DISTINCT items.title FROM items
+        INNER JOIN reviews ON items.id = reviews.item_id
+        WHERE items.id IN (" . implode(",", $item_ids) . ")
+        AND (
+            reviews.review LIKE '%Excellent%' OR
+            reviews.review LIKE '%Good%'
+        )
+        AND items.id NOT IN (
+            SELECT item_id FROM reviews
+            WHERE review LIKE '%Bad%' OR review LIKE '%Fair%'
+        )";
 
 
+        //  Use: neil, items 2(ipad/poor, excellent,good) and 6(google pixel 6/good, exccelent) | gary, items 4(Apple TV/excellent, excellent) and 5(Samsung 23/poor) | rick for no items with good or excellent ratings.
 
-    // Execute the query
-    $result = $link->query($sql);
+        // $sql = "SELECT DISTINCT items.title FROM items
+        // INNER JOIN reviews ON items.id = reviews.item_id
+        // WHERE items.id IN (" . implode(",", $item_ids) . ")
+        // AND reviews.rating IN ('excellent', 'good')
+        // AND items.id NOT IN (
+        //     SELECT item_id FROM reviews
+        //     WHERE rating IN ('fair', 'poor')
+        // )";
 
 
-    if (mysqli_num_rows($result) > 0) {
-        echo "<h2 style='text-align: center; text-decoration: underline;'>Items with only excellent or good rating belonging to user " . $item_username . "</h2>";
-        echo "<div style='margin: 0 auto; max-width: 800px;'>";
-        echo "<table style='border-collapse: collapse; width: 100%;'>";
-        echo "<tr style='background-color: #ddd;'><th style='padding: 10px; border: 1px solid #000;'>Item</th></tr>";
-        while ($row = mysqli_fetch_assoc($result)) {
-            echo "<tr><td style='padding: 10px; border: 1px solid #000;'>" . $row["title"] . "</td></tr>";
+        // Execute the query
+        $result = $link->query($sql);
+
+        if (mysqli_num_rows($result) > 0) {
+            echo "<h2 style='text-align: center; text-decoration: underline;'>Items with only excellent or good rating belonging to user " . $item_username . "</h2>";
+            echo "<div style='margin: 0 auto; max-width: 800px;'>";
+            echo "<table style='border-collapse: collapse; width: 100%;'>";
+            echo "<tr style='background-color: #ddd;'><th style='padding: 10px; border: 1px solid #000;'>Item</th></tr>";
+            while ($row = mysqli_fetch_assoc($result)) {
+                echo "<tr><td style='padding: 10px; border: 1px solid #000;'>" . $row["title"] . "</td></tr>";
+            }
+            echo "</table>";
+            echo "</div>";
+        } else {
+            echo "<p style='text-align: center;'>No items found with only excellent or good rating belonging to user " . $item_username . ".</p>";
         }
-        echo "</table>";
-        echo "</div>";
     } else {
-        echo "<p style='text-align: center;'>No items found with only excellent or good rating belonging to user " . $item_username . ".</p>";
+        echo "<p style='text-align: center;'>The username " . $item_username . " does not exist in the database.</p>";
     }
 }
 
